0314ff1a79027e661e15d9a76b33f6ea82c65f75
Comfort Mwalija <calmwalija@gmail.com>
2022-04-12 08:51:27 +0200

    Enhancements 1. Order by number as integer 2. Firebase cloud messaging together with service

--- app/src/main/java/net/techandgraphics/hymn/HymnApplication.kt
+++ app/src/main/java/net/techandgraphics/hymn/HymnApplication.kt
@@ -1,7 +1,9 @@
 package net.techandgraphics.hymn
 
 import android.app.Application
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationManagerCompat
@@ -10,22 +12,26 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class HymnApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        onCreateNotificationChannel()
+    }
     companion object {
         const val NOTIFICATION_ID = "198193"
         const val NOTIFICATION_DESCRIPTION = "Hymn Channel Description"
     }
 
-    private fun createNotificationChannel() {
+    private fun onCreateNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannelCompat.Builder(
-                NOTIFICATION_ID,
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                setDescription(NOTIFICATION_DESCRIPTION)
-                setVibrationEnabled(true)
-                NotificationManagerCompat.from(this@HymnApplication)
-                    .createNotificationChannel(this.build())
-            }
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(
+                    NotificationChannel(
+                        NOTIFICATION_ID,
+                        NOTIFICATION_DESCRIPTION,
+                        NotificationManager.IMPORTANCE_HIGH
+                    )
+                )
         }
     }
-}
+
+ }
