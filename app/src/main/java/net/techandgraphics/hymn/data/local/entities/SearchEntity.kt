81e317fd42c71559124ac9ac01f04e9e2f925421
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2023-12-19 10:57:48 +0200

    UI Layer: Search Screen Enhancements (#34)

--- app/src/main/java/net/techandgraphics/hymn/data/local/entities/SearchEntity.kt
+++ app/src/main/java/net/techandgraphics/hymn/data/local/entities/SearchEntity.kt
@@ -1,11 +1,12 @@
 package net.techandgraphics.hymn.data.local.entities
 
 import androidx.room.Entity
+import net.techandgraphics.hymn.data.local.Lang
 
 @Entity(primaryKeys = ["query"], tableName = "Search")
 data class SearchEntity(
   val query: String,
   val tag: String,
-  val lang: String = "en",
+  val lang: String = Lang.EN.name,
   val id: Long = System.currentTimeMillis(),
 )
