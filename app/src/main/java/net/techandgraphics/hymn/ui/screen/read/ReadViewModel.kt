fa09d34f7e64c352d123ca12e84a776af29ff555
Comfort Mwalija <calmwalija@gmail.com>
2023-12-22 17:45:38 +0200

    UI Layer: Read Screen Change Lyrics Font Size

--- app/src/main/java/net/techandgraphics/hymn/ui/screen/read/ReadViewModel.kt
+++ app/src/main/java/net/techandgraphics/hymn/ui/screen/read/ReadViewModel.kt
@@ -1,23 +1,32 @@
 package net.techandgraphics.hymn.ui.screen.read
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import net.techandgraphics.hymn.R
 import net.techandgraphics.hymn.data.local.Database
 import net.techandgraphics.hymn.data.local.entities.LyricEntity
+import net.techandgraphics.hymn.data.prefs.UserPrefs
 import javax.inject.Inject
 
 @HiltViewModel
 class ReadViewModel @Inject constructor(
   private val database: Database,
   private val version: String,
+  private val sharedPreferences: SharedPreferences,
+  private val userPrefs: UserPrefs,
 ) : ViewModel() {
 
   private val _state = MutableStateFlow(ReadState())
   val state = _state.asStateFlow()
+
+  private var fontJob: Job? = null
 
   operator fun invoke(id: Int) = viewModelScope.launch {
     var index = 1
@@ -28,7 +37,11 @@ class ReadViewModel @Inject constructor(
           lyric = lyric
         )
       }.also {
-        _state.value = _state.value.copy(lyricEntityKey = it)
+        _state.value = _state.value.copy(
+          lyricEntityKey = it,
+          fontSize = sharedPreferences
+            .getInt(userPrefs.context.getString(R.string.font_key), 2)
+        )
       }
   }
 
@@ -49,6 +62,20 @@ class ReadViewModel @Inject constructor(
       is ReadEvent.Favorite -> favorite(event.data)
       is ReadEvent.Click -> Unit
       is ReadEvent.Read -> read(event.data)
+      is ReadEvent.FontSize -> fontSize(event.size)
+    }
+  }
+
+  private fun fontSize(font: Int) {
+    _state.value = _state.value.copy(fontSize = font)
+    fontJob = viewModelScope.launch {
+      fontJob?.cancel()
+      delay(1000)
+      sharedPreferences.edit().putInt(userPrefs.context.getString(R.string.font_key), font).apply()
+      _state.value = _state.value.copy(
+        fontSize = sharedPreferences
+          .getInt(userPrefs.context.getString(R.string.font_key), font)
+      )
     }
   }
 
