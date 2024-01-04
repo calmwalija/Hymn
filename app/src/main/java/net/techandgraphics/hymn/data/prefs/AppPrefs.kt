e79fb5136302aeb87067b5c45ea3bca04105cd9d
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2024-01-04 11:19:36 +0200

    Bug Fix: Datastore keys and Splashscreen Revamp (#96)

--- app/src/main/java/net/techandgraphics/hymn/data/prefs/AppPrefs.kt
+++ app/src/main/java/net/techandgraphics/hymn/data/prefs/AppPrefs.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import net.techandgraphics.hymn.R
 import javax.inject.Inject
@@ -25,27 +23,17 @@ class AppPrefs @Inject constructor(
   private val dataStore = context.dataStore
 
   companion object {
-    const val JSON_BUILD = 2
+    const val JSON_BUILD_KEY = 2.toString()
   }
 
-  val jsonBuild = intPreferencesKey(context.getString(R.string.json_build_key))
+  val jsonBuildKey = context.getString(R.string.json_build_key)
   val fontKey = context.getString(R.string.font_key)
 
   suspend fun setPrefs(key: String, value: String) {
     dataStore.edit { it[stringPreferencesKey(key)] = value }
   }
 
-  suspend fun getPrefs(key: String): String? {
-    return dataStore.data.first()[stringPreferencesKey(key)]
-  }
-
-  fun getPrefsAsFlow(key: String): Flow<String?> {
+  fun getPrefs(key: String): Flow<String?> {
     return dataStore.data.map { it[stringPreferencesKey(key)] }
   }
-  suspend fun getPrefsSafe(key: String): String {
-    return dataStore.data.first()[stringPreferencesKey(key)]!!
-  }
-
-  suspend fun setJsonBuild(build: Int) = dataStore.edit { it[jsonBuild] = build }
-  val getJsonBuild = dataStore.data.map { it[jsonBuild] ?: 1 }
 }
