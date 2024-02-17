4a7df0b18e6c2ac2a4145d4baf86daf8c3a16e3a
Comfort Mwalija <calmwalija@gmail.com>
2024-02-17 11:34:37 +0200

    Architecture Enhancement: Remove Search Pagination

--- app/src/main/java/net/techandgraphics/hymn/ui/screen/search/SearchState.kt
+++ app/src/main/java/net/techandgraphics/hymn/ui/screen/search/SearchState.kt
@@ -1,13 +1,10 @@
 package net.techandgraphics.hymn.ui.screen.search
 
-import androidx.paging.PagingData
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import net.techandgraphics.hymn.domain.model.Lyric
 import net.techandgraphics.hymn.domain.model.Search
 
 data class SearchState(
-  val lyricsPaged: Flow<PagingData<Lyric>> = emptyFlow(),
+  val lyrics: List<Lyric> = emptyList(),
   val search: List<Search> = emptyList(),
   var searchQuery: String = "",
   var isSearching: Boolean = false,
