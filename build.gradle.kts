cfa7864f6bbda8c9f0b89943f50a0246c977752d
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2024-05-23 18:15:40 +0200

    Architecture Enhancements: Dependencies and Android Gradle plugin Update (#138)
    * Architecture Enhancements: Dependencies and Android Gradle plugin Update
    
    * Bug fix
    Unresolved Reference: coil
    
    * Error: Class referenced in the manifest, androidx.startup.InitializationProvider, was not found in the project or the libraries [MissingClass]
    
    * Reason: Task ':app:uploadCrashlyticsMappingFileRelease' uses this output of task ':app:processDebugGoogleServices' without declaring an explicit or implicit dependency. This can lead to incorrect results being produced, depending on what order the tasks are executed.
    
    * Bug fix
    app:uploadCrashlyticsMappingFileRelease

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,16 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:8.2.1")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-    classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-    classpath("com.diffplug.spotless:spotless-plugin-gradle:6.6.0")
-    classpath("com.google.gms:google-services:4.4.0")
-    classpath("com.google.firebase:perf-plugin:1.4.2")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    classpath(libs.spotless.plugin.gradle)
+    classpath(libs.hilt.android.gradle.plugin)
+    classpath(libs.google.services)
+    classpath(libs.perf.plugin)
+    classpath(libs.firebase.crashlytics.gradle)
   }
 }
 
 plugins {
-  id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+  alias(libs.plugins.androidApplication) apply false
+  alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+  alias(libs.plugins.hiltAndroid) apply false
 }
