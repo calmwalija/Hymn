0660c58b9b3a6d274264ce2fc2f4b4eacb354871
Comfort Mwalija <calmwalija@gmail.com>
2025-01-21 10:36:00 +0200

    Fix Toolbar Top Margin Issue on Android 15 and Refactor FontDialog into Separate File

--- app/src/main/java/net/techandgraphics/hymn/ui/screen/preview/PreviewScreen.kt
+++ app/src/main/java/net/techandgraphics/hymn/ui/screen/preview/PreviewScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -181,8 +182,8 @@ fun PreviewScreen(
             )
           }
         },
-        modifier = Modifier
-          .shadow(elevation = 8.dp),
+        windowInsets = WindowInsets(top = 0.dp),
+        modifier = Modifier.shadow(elevation = 8.dp),
       )
     },
   ) { paddingValues ->
